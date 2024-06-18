8150295573e25d22b08175cbac8f2b8815a03ea8
Dylan Evans <62859381+fin3ss3g0d@users.noreply.github.com>
2024-06-17 20:54:19 -0400

    update shiro-hash-generator

--- shiro-hash-generator/src/main/java/com/example/ShiroHashingExample.java
+++ shiro-hash-generator/src/main/java/com/example/ShiroHashingExample.java
@@ -16,15 +16,31 @@ public class ShiroHashingExample {
         return Base64.getEncoder().encodeToString(salt);
     }
 
-    public static String hashPassword(String password, String salt, int iterations) {
-        ByteSource saltBytes = ByteSource.Util.bytes(Base64.getDecoder().decode(salt));
-        Sha512Hash hash = new Sha512Hash(password, saltBytes, iterations);
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    public static String hashPassword(String password, String salt, int iterations, boolean isHex) {
+        byte[] saltBytes;
+        if (isHex) {
+            saltBytes = hexStringToByteArray(salt);
+        } else {
+            saltBytes = Base64.getDecoder().decode(salt);
+        }
+        ByteSource saltByteSource = ByteSource.Util.bytes(saltBytes);
+        Sha512Hash hash = new Sha512Hash(password, saltByteSource, iterations);
         return Base64.getEncoder().encodeToString(hash.getBytes());
     }
 
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.err.println("Usage: java -jar shiro-hash.jar <string-to-hash> <iterations>");
+        if (args.length < 2 || args.length > 3) {
+            System.err.println("Usage: java -jar shiro-hash.jar <string-to-hash> <iterations> [hex-salt]");
             System.exit(1);
         }
 
@@ -38,9 +54,19 @@ public class ShiroHashingExample {
             return;
         }
 
-        String salt = generateSalt();
-        String hashedPassword = hashPassword(password, salt, iterations);
-        System.out.println("$shiro1$SHA-512$" + iterations + "$" + salt + "$" + hashedPassword);
+        String salt;
+        String hashedPassword;
+        if (args.length == 3) {
+            salt = args[2];
+	    byte[] saltBytes;
+	    saltBytes = hexStringToByteArray(salt);
+            hashedPassword = hashPassword(password, salt, iterations, true);
+            System.out.println("$shiro1$SHA-512$" + iterations + "$" + Base64.getEncoder().encodeToString(saltBytes) + "$" + hashedPassword);
+        } else {
+            salt = generateSalt();
+            hashedPassword = hashPassword(password, salt, iterations, false);
+            System.out.println("$shiro1$SHA-512$" + iterations + "$" + salt + "$" + hashedPassword);
+        }
     }
 }
 
